e78c8e8bfadf0e99a0dbf48260251f5f596ac3c8
takashiyamaguchi <tkshyamaguchi45@gmail.com>
2015-02-15 23:36:51 +0900

    foundation iconに変更

--- js/app.js
+++ js/app.js
@@ -2,27 +2,27 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-$(function(){
+// $(function(){
 
-	var win = $( window );
+// 	var win = $( window );
 
-	var viewSize = function(){
+// 	var viewSize = function(){
 
-		var
-			viewImage = $( '.cb-view-wrap' ), 
-			viewWidth = viewImage.width()
-		;
+// 		var
+// 			viewImage = $( '.cb-view-wrap' ), 
+// 			viewWidth = viewImage.width()
+// 		;
 
-		//.cb-viewの幅を取得して高さに反映。正方形にする
-		viewImage.css( 'height', viewWidth );		
-	};
+// 		//.cb-viewの幅を取得して高さに反映。正方形にする
+// 		viewImage.css( 'height', viewWidth );		
+// 	};
 
 
 
-	//初回アクセス時
-	viewSize();
+// 	//初回アクセス時
+// 	viewSize();
 
-	//リサイズ時
-	win.on( 'resize', viewSize )
+// 	//リサイズ時
+// 	win.on( 'resize', viewSize )
 
-});
+// });

14778f15c0e24f66832d39ba5bcb3da370c967da
takashiyamaguchi <tkshyamaguchi45@gmail.com>
2015-02-18 21:04:36 +0900

    記事レイアウトの修正

--- js/app.js
+++ js/app.js
@@ -2,27 +2,8 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-// $(function(){
-
-// 	var win = $( window );
-
-// 	var viewSize = function(){
-
-// 		var
-// 			viewImage = $( '.cb-view-wrap' ), 
-// 			viewWidth = viewImage.width()
-// 		;
-
-// 		//.cb-viewの幅を取得して高さに反映。正方形にする
-// 		viewImage.css( 'height', viewWidth );		
-// 	};
-
-
-
-// 	//初回アクセス時
-// 	viewSize();
-
-// 	//リサイズ時
-// 	win.on( 'resize', viewSize )
-
-// });
+$(document).ready(function() {
+ 
+	$("#owl-example").owlCarousel();
+ 
+});

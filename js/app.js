5cbeeb0317a7f64319ac13086e9b1389e07cc352
takashiyamaguchi <tkshyamaguchi45@gmail.com>
2015-04-26 15:41:33 +0900

    test

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
-
-$(document).ready(function() {
- 
-	$("#owl-example").owlCarousel();
- 
-});

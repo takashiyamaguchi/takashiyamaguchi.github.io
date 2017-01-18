2673e4c1d535400a6fe4aae643336c70a8aac4ed
bitbeans_yamaguchi <yamaguchi@bitbeans.com>
2017-01-18 21:22:48 +0900

    sassつながった

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
-/*
-var paths = {
-  'scss': 'scss',
-  'css': 'css'
-}
-*/
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
-gulp.task('sass', function(){
-	gulp.src('scss/**/**.scss')
-	.pipe(sass())
-	.pipe(gulp.dest('./css')); // 出力先
+    browserSync.init({
+        server: {
+        	baseDir: "./"
+        }
+    });
+
+    gulp.watch("./scss/**/*.scss", ['sass']);
+    gulp.watch("./**/*.html").on('change', browserSync.reload);
 });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./scss/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./css/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);

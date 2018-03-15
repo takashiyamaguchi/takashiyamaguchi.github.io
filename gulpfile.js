3a4b395a4c05fa1dee9ad4c1f5b81cc54eaa10f9
bitbeans_yamaguchi <yamaguchi@bitbeans.com>
2018-03-16 03:22:29 +0900

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+// module
+// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var browserSync  = require('browser-sync').create();
+var ssi          = require('browsersync-ssi');
+var cssnext      = require('postcss-cssnext');
 
-// Static Server + watching scss/html files
+// ディレクトリ : srcがinput distがoutput
+// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+var dir = {
+  src: {
+    css  : 'src/css',
+    js   : 'src/js',
+    aigis: 'src/aigis'
+  },
+  dist: {
+    css  : 'dist/css',
+    js   : 'dist/js',
+    aigis: 'dist/aigis'
+  }
+};
+
+// ローカルサーバ立ち上げ、watch
+// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 gulp.task('serve', ['css'], function() {
 
     browserSync.init({
@@ -15,12 +38,35 @@ gulp.task('serve', ['css'], function() {
     gulp.watch("./**/*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
+// sassのコンパイル & autoprefix
+// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 gulp.task('css', function() {
+    var processors = [
+        cssnext({browser: [
+          'last 2 versions', // 最新２バージョン
+          'ie >= 10',        // IE10以上
+          'iOS >= 8',        // iOS8以上
+          'Android >= 4'     // Android4以上
+        ]})
+    ];
     return gulp.src("./src/css/**/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./dist/css/"))
+        .pipe(sourcemaps.init())
+            .pipe(sass()).on('error', sass.logError)
+            .pipe(postcss(processors))
+        .pipe(sourcemaps.write('/maps'))
+        .pipe(gulp.dest(dir.dist.css))
         .pipe(browserSync.stream());
 });
 
+
 gulp.task('default', ['serve']);
+
+
+
+
+
+
+
+
+
+

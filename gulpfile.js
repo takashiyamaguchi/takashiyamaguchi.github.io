7fb46937e8724eec588fd9eb265bb2d1faee5607
TakashiYamaguchi <tkshyamaguchi45@gmail.com>
2018-03-18 20:45:45 +0900

    aigisいれた

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
+'use strict';
+
 // module
 // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var postcss      = require('gulp-postcss');
+var aigis        = require('gulp-aigis');
 var autoprefixer = require('autoprefixer');
 var browserSync  = require('browser-sync').create();
 var ssi          = require('browsersync-ssi');
 var cssnext      = require('postcss-cssnext');
+var runSequence  = require('run-sequence');
+
 
 // ディレクトリ : srcがinput distがoutput
 // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
@@ -24,9 +29,10 @@ var dir = {
   }
 };
 
+
 // ローカルサーバ立ち上げ、watch
 // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-gulp.task('serve', ['css'], function() {
+gulp.task('serve', function() {
 
     browserSync.init({
         server: {
@@ -34,9 +40,10 @@ gulp.task('serve', ['css'], function() {
         }
     });
 
-    gulp.watch("./src/css/**/*.scss", ['css']);
+    // gulp.watch("./src/css/**/*.scss", ['css']);
     gulp.watch("./**/*.html").on('change', browserSync.reload);
 });
+
 
 // sassのコンパイル & autoprefix
 // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
@@ -51,7 +58,9 @@ gulp.task('css', function() {
     ];
     return gulp.src("./src/css/**/*.scss")
         .pipe(sourcemaps.init())
-            .pipe(sass()).on('error', sass.logError)
+            .pipe(sass({
+              outputStyle: 'expanded'
+            })).on('error', sass.logError)
             .pipe(postcss(processors))
         .pipe(sourcemaps.write('/maps'))
         .pipe(gulp.dest(dir.dist.css))
@@ -59,14 +68,58 @@ gulp.task('css', function() {
 });
 
 
-gulp.task('default', ['serve']);
+// スタイルガイドの作成
+// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+gulp.task('aigis:css', function() {
+  var processors = [
+      cssnext({browser: [
+        'last 2 versions', // 最新２バージョン
+        'ie >= 10',        // IE10以上
+        'iOS >= 8',        // iOS8以上
+        'Android >= 4'     // Android4以上
+      ]})
+  ];
+  return gulp.src("./src/css/**/*.scss")
+    .pipe(sass({
+      outputStyle: 'expanded',
+    })).on('error', sass.logError)
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(dir.dist.aigis + '/aigis_assets/css'));
+});
 
 
+// スタイルガイドの作成
+// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+gulp.task('aigis:update', function() {
+  return _aigis();
+});
+gulp.task('aigis:build', function() {
+  return _aigis();
+});
+function _aigis() {
+  return gulp.src(dir.src.aigis + '/aigis_config.yml')
+    .pipe(aigis())
+    .on('end', function() {
+      runSequence('aigis:css');
+    });
+}
 
 
+// watch系のタスク
+// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+gulp.task('watch', function() {
+  gulp.watch([dir.src.css + '/**/*.scss'], function() {
+    runSequence('css', 'aigis:update');
+  });
+
+  gulp.watch([dir.src.aigis + '/**/*.ejs'], function() {
+    runSequence('aigis:update');
+  });
+
+  gulp.watch([dir.src.aigis + '/aigis_assets/css/**/*.scss'], function() {
+    runSequence('aigis:css');
+  });
+});
 
 
-
-
-
-
+gulp.task('default', ['watch', 'serve']);

e545632ca7e2273d048faffa30b3cd8e9004e9f8
yamaguchitakashi <yamaguchi@bitbeans.com>
2020-01-07 17:57:22 +0900

    sassの追加

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,13 +1,47 @@
-var gulp = require("gulp");
-var rename = require("gulp-rename");
-var ejs = require("gulp-ejs");
-var replace = require("gulp-replace");
+'use strict';
 
-gulp.task("ejs", (done) => {
-  gulp
-    .src(["ejs/**/*.ejs", "!" + "ejs/**/_*.ejs"])
-    .pipe(ejs({}, {}, {ext:'.html'}))
-    .pipe(rename({ extname: ".html" }))
-    .pipe(gulp.dest("./"));
-  done();
-});
+const { src, dest, watch, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const ejs = require('gulp-ejs');
+const replace = require('gulp-replace');
+
+sass.compiler = require('node-sass');
+
+// path
+const srcPath = {
+    html: [
+        'ejs/**/*.ejs',
+        '!' + 'ejs/**/_*.ejs'
+    ],
+    css: [
+        'scss/**/*.scss'
+    ]
+}
+
+const distPath = {
+    html: 'ejs/**/*.ejs',
+    css: 'scss/**/*.scss'
+}
+
+// sass compile
+const css = () => {
+    return src(srcPath.css)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('./css'));
+}
+
+// ejs compile
+const html = () => {
+    return src(srcPath.html)
+        .pipe(ejs({}, {}, {ext:'.html'}))
+        .pipe(rename({ extname: '.html' }))
+        .pipe(dest('./'));
+}
+
+exports.default = () => {
+    watch(distPath.html, html)
+    watch(distPath.css, css)
+}
+
+exports.build = parallel(html, css)
